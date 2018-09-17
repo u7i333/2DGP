600ffa891714d9993a7f6283239d01b4f20cb2ff
hyojunlee(notebook) <u7i333@naver.com>
2018-09-17 17:08:50 +0900

    drill-05
    캐릭터가 갈 경로 좌표에 따라 함수로 나눔

--- Drills/Drills-05/drill-05.py
+++ Drills/Drills-05/drill-05.py
@@ -1,13 +1,48 @@
 from pico2d import *
 
-def move_character():
-    pass
+
 
 open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-while True:
+def move_to_203_535():
+    pass
+def move_to_132_243():
+    pass
+def move_to_535_470():
+    pass
+def movo_to_477_203():
+    pass
+def movo_to_715_136():
+    pass
+def movo_to_316_225():
+    pass
+def movo_to_510_92():
+    pass
+def movo_to_692_518():
+    pass
+def movo_to_682_332():
+    pass
+def movo_to_712_349():
+    pass
+def movo_to_start():
+    pass
+
+def move_character():
+    move_to_203_535()
+    move_to_132_243()
+    move_to_535_470()
+    movo_to_477_203()
+    movo_to_715_136()
+    movo_to_316_225()
+    movo_to_510_92()
+    movo_to_692_518()
+    movo_to_682_332()
+    movo_to_712_349()
+    movo_to_start()
+
+while(True):
     move_character()
 
 close_canvas()
