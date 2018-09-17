2a6dc4430d1d15472f5c78a411e3e08cd022d1fd
hyojunlee(notebook) <u7i333@naver.com>
2018-09-17 21:29:53 +0900

    drill-05
    9번째점에서 10번째점으로 이동

--- Drills/Drills-05/drill-05.py
+++ Drills/Drills-05/drill-05.py
@@ -171,7 +171,22 @@ def movo_to_682_332():
 
 
 def movo_to_712_349():
-    pass
+    fromx, fromy = 682, 332
+    tox, toy = 712, 349
+    incre_or_decre_x = (tox - fromx) / 50
+    incre_or_decre_y = (toy - fromy) / 50
+    frame = 0
+
+    while (fromx < tox and fromy < toy):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, fromx, fromy)
+        update_canvas()
+        frame = (frame + 1) % 8
+        fromx += incre_or_decre_x
+        fromy += incre_or_decre_y
+        delay(0.05)
+        get_events()
 
 def movo_to_start():
     pass
@@ -185,7 +200,7 @@ def move_character():
     #movo_to_316_225()
     #movo_to_510_92()
     #movo_to_692_518()
-    movo_to_682_332()
+    #movo_to_682_332()
     movo_to_712_349()
     movo_to_start()
 
