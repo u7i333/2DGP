2f479b79d91c0e56c34eb3034788f4924ed4308a
hyojunlee(notebook) <u7i333@naver.com>
2018-09-20 16:33:13 +0900

    lab-05
    강의 진행

--- Drills/Drills-05/move_character_with_key.py
+++ Drills/Drills-05/move_character_with_key.py
@@ -1,9 +1,28 @@
 from pico2d import *
 
-
 def handle_events():
-    # fill here
-    pass
+    global running
+    global dir
+    global facelook
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+                facelook = 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+                facelook = 0
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
 
 
 open_canvas()
@@ -13,16 +32,17 @@ character = load_image('animation_sheet.png')
 running = True
 x = 800 // 2
 frame = 0
+dir = 0
+facelook = 1
 
 while running:
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
+    character.clip_draw(frame * 100, 100*facelook, 100, 100, x, 90)
     update_canvas()
-
     handle_events()
     frame = (frame + 1) % 8
-
+    x += dir *5
     delay(0.05)
 
 close_canvas()
