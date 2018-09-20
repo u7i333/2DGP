0a01a631c837254f990c85fb48ad7a49e1a21031
hyojunlee(notebook) <u7i333@naver.com>
2018-09-20 17:03:23 +0900

    drill-06
    캐릭터 움직임 함수 뼈대

--- Drills/Drills-05/drill-06.py
+++ Drills/Drills-05/drill-06.py
@@ -2,15 +2,22 @@ from pico2d import *
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
+def move_characger():
+    pass
+
 def handle_events():
     global running
     global mousex, mousey
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
             mousex, mousey = event.x, KPU_HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            mousex, mousey = event.x, KPU_HEIGHT - 1 - event.y
+            move_character()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
