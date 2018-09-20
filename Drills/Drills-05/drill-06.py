5ce5d28f9820ff60719e8cfd40999b51f7ead2b4
hyojunlee(notebook) <u7i333@naver.com>
2018-09-20 17:27:59 +0900

    drill-06 완성
    2014180056 이효준
    총 커밋회수 : 7
    https://youtu.be/H1jJHRRHic0

--- Drills/Drills-05/drill-06.py
+++ Drills/Drills-05/drill-06.py
@@ -2,13 +2,14 @@ from pico2d import *
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
-def move_characger():
-    pass
-
 def handle_events():
     global running
     global mousex, mousey
+    global movex, movey
+    global x,y
+    global frame
 
+    count = 0
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -17,7 +18,22 @@ def handle_events():
             mousex, mousey = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN:
             mousex, mousey = event.x, KPU_HEIGHT - 1 - event.y
-            move_character()
+            movex = mousex - x
+            movey = mousey - y
+            while count < 10:
+                clear_canvas()
+                kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+                if(movex > 0):
+                    character.clip_draw(frame * 100, 100 , 100, 100, x, y)
+                else:
+                    character.clip_draw(frame * 100, 0, 100, 100, x, y)
+                x = x + movex/ 10
+                y = y + movey/ 10
+                count = count +1
+                frame = (frame + 1) % 8
+                update_canvas()
+                delay(0.02)
+
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
@@ -28,9 +44,11 @@ mousecursor = load_image('hand_arrow.png')
 
 running = True
 mousex, mousey = KPU_WIDTH // 2, KPU_HEIGHT // 2
+global x, y
 x,y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 hide_cursor()
+
 
 while running:
     clear_canvas()
@@ -41,6 +59,7 @@ while running:
     frame = (frame + 1) % 8
     delay(0.02)
     handle_events()
+
 
 close_canvas()
 
