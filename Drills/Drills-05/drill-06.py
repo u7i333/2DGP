949e74bd6f2f8eec6c5e6a6971830b221c35e1f5
hyojunlee(notebook) <u7i333@naver.com>
2018-09-20 16:48:42 +0900

    drill-06
    기본적인 뼈대 완성

--- Drills/Drills-05/drill-06.py
+++ Drills/Drills-05/drill-06.py
@@ -3,20 +3,12 @@ from pico2d import *
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def handle_events():
-    global running
-    global x, y
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, KPU_HEIGHT - 1 - event.y
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+    pass
 
-open_canvas(KPU_WIDTH,KPU_HEIGHT)
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
-character = load_image('animation_sheet.png')
+character = load_image('run_animation.png')
+mousecursor = load_image('hand_arrow.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
@@ -33,7 +25,4 @@ while running:
     handle_events()
 
 close_canvas()
-
-
-
 
