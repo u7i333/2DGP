443b380e7399c08513429bffb07e452be3353cc3
hyojunlee(notebook) <u7i333@naver.com>
2018-09-20 16:57:08 +0900

    drill-06
    화면 정가운데에 캐릭터 생성

--- Drills/Drills-05/drill-06.py
+++ Drills/Drills-05/drill-06.py
@@ -4,30 +4,32 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def handle_events():
     global running
-    global x, y
+    global mousex, mousey
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, KPU_HEIGHT - 1 - event.y
+            mousex, mousey = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
-character = load_image('run_animation.png')
+character = load_image('animation_sheet.png')
 mousecursor = load_image('hand_arrow.png')
 
 running = True
-x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+mousex, mousey = KPU_WIDTH // 2, KPU_HEIGHT // 2
+x,y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    mousecursor.draw(x, y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    mousecursor.draw(mousex, mousey)
     update_canvas()
     frame = (frame + 1) % 8
     delay(0.02)
