ea1eac39c301b938af8abfe77577cd6dc2e9253a
hyojunlee(notebook) <u7i333@naver.com>
2018-10-04 19:56:48 +0900

    drill-08-02
    Canvas 함수 KPU배경화면 출력

--- Drills/Lecture06/drill-08-2.py
+++ Drills/Lecture06/drill-08-2.py
@@ -10,15 +10,13 @@ character = load_image('animation_sheet.png')
 
 x,y = KPU_WIDTH//2, KPU_HEIGHT//2
 
-points = [(random.randint(0,KPU_WIDTH),random.randint(0,KPU_HEIGHT))]
-
-def Move_Rand
+def Move_Rand():
+    pass
 
 while True:
     clear_canvas()
     KPU_Ground.draw(KPU_WIDTH//2, KPU_HEIGHT//2)
     update_canvas()
-
     delay(0.02)
 
 close_canvas()
