3d746c2aff8a86fdbbd7e2f877d62447e2d3f4b5
Lee Hyo Jun <u7i333@naver.com>
2018-10-15 16:58:51 +0900

    lecture10
    10-15 강의 내용

--- Lebs/Lecture10/boy.py
+++ Lebs/Lecture10/boy.py
@@ -1,10 +1,10 @@
 from pico2d import *
 
 # Boy State
-# fill here
+IDLE, RUN = range(2)
 
 # Boy Event
-# fill here
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP = range(4)
 
 key_event_table = {
     (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
@@ -14,13 +14,9 @@ key_event_table = {
 }
 
 next_state_table = {
-# fill here
+    IDLE: {RIGHT_UP: RUN, LEFT_UP: RUN, RIGHT_DOWN: RUN,LEFT_DOWN: RUN},
+    RUN: {RIGHT_UP: IDLE, LEFT_UP: IDLE, RIGHT_DOWN: IDLE,LEFT_DOWN: IDLE}
 }
-
-
-
-
-
 
 
 class Boy:
@@ -45,26 +41,29 @@ class Boy:
         pass
 
     def do_IDLE(self):
-        # fill here
-        pass
+        self.frame = (self.frame + 1) % 8
+        self.timer -= 1
 
     def draw_IDLE(self):
-        # fill here
-        pass
-
+        if self.dir == 1:
+            self.image.clip_draw(self.frame * 100, 300, 100, 100, self.x, self.y)
+        else:
+            self.image.clip_draw(self.frame * 100, 200, 100, 100, self.x, self.y)
 
 
     # RUN state functions
     def enter_RUN(self):
-        # fill here
-        pass
+        self.frame = 0
+        self.dir = self.velocity
 
     def exit_RUN(self):
         pass
 
     def do_RUN(self):
-        # fill here
-        pass
+        self.frame = (self.frame + 1) % 8
+        self.x += self.velocity
+        self.x = clamp(25, self.x, 800-25)
+
 
     def draw_RUN(self):
         if self.velocity == 1:
@@ -97,20 +96,25 @@ class Boy:
 
 
     def change_state(self,  state):
-        # fill here
-        pass
+        self.exit_state[self.cur_state](self)
+        self.enter_state[state](self)
+        self.cur_state = state
 
     enter_state = {IDLE: enter_IDLE, RUN: enter_RUN}
-    # fill here
+    exit_state = {IDLE: exit_IDLE, RUN: exit_RUN}
+    do_state = {IDLE: do_IDLE, RUN: do_RUN}
+    draw_state = {IDLE: draw_IDLE, RUN: draw_RUN}
 
 
     def update(self):
-        # fill here
-        pass
+        self.do_state[self.cur_state](self)
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.change_state(next_state_table[self.cur_state][event])
+
 
     def draw(self):
-        # fill here
-        pass
+        self.draw_state[self.cur_state](self)
 
 
     def handle_event(self, event):
