8f65165025b2a0e2ca5bbe359ec874a57e4b101d
Lee Hyo Jun <u7i333@naver.com>
2018-11-19 17:06:33 +0900

    drill-13
    stamp 써서 이동 영역 제한

--- Lebs/Lecture15/boy.py
+++ Lebs/Lecture15/boy.py
@@ -71,8 +71,8 @@ class WalkingState:
         boy.y += boy.y_velocity * game_framework.frame_time
 
         # fill here
-        #boy.x = clamp(0,boy.x,boy.bg.w)
-        #boy.y = clamp(0, boy.y, boy.bg.h)
+        boy.x = clamp((PIXEL_PER_METER*7) / boy.bg.h * boy.y, boy.x, boy.bg.w - (PIXEL_PER_METER * 7) / boy.bg.h *boy.y)
+        boy.y = clamp(PIXEL_PER_METER * 2, boy.y, boy.bg.h - PIXEL_PER_METER * 2)
 
     @staticmethod
     def draw(boy):
